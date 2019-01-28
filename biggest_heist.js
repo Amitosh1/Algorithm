e79f0c3f7f482a8c654a3103a4753f094a7b4a69
Mishra <amimishra@paypal.com>
2019-01-28 00:50:27 -0600

    biggest heist

---
+++ biggest_heist.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var rob = function(nums, start=0) {
+    var indx = 0;
+    var length = nums.length
+    var solutions = {};
+    while(indx<length){
+        if(indx === 0){
+            solutions[0] = nums[indx];
+        }else if(indx ===1){
+            solutions[1] = nums[0]>nums[1]? nums[0]:nums[1];
+        }else{
+            solutions[indx]=Math.max(nums[indx]+solutions[indx-2],solutions[indx-1]);
+        }
+        indx++;
+    }
+    return solutions[nums.length-1] || 0;
+};
